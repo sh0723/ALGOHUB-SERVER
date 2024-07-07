1c6e1e527d68bd9b90023d638dc129729df7f626
rladmstn <oz33177351@gmail.com>
2024-07-07 18:23:49 +0900

    init : 초기 세팅

---
+++ src/main/java/com/gamzabat/algohub/AlgohubApplication.java
@@ -0,0 +1,13 @@
+package com.gamzabat.algohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamzabat/algohub/AlgohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamzabat.algohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
