42efe0bc985090ab9a643f78d4d3a1d7dc547488
rladmstn <oz33177351@gmail.com>
2024-07-09 20:29:09 +0900

    test : 초대 코드를 이용한 그룹 참여 테스트 작성
    초대 코드를 이용한 그룹 참여 성공, 존재하지 않는 그룹, 이미 참여한 그룹으로 인한 실패 테스트 작성

--- src/test/java/com/gamzabat/algohub/service/StudyGroupServiceTest.java
+++ src/test/java/com/gamzabat/algohub/service/StudyGroupServiceTest.java
@@ -3,6 +3,9 @@ package com.gamzabat.algohub.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,9 +17,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
+import com.gamzabat.algohub.domain.GroupMember;
 import com.gamzabat.algohub.domain.StudyGroup;
 import com.gamzabat.algohub.domain.User;
 import com.gamzabat.algohub.enums.Role;
+import com.gamzabat.algohub.exception.StudyGroupValidationException;
+import com.gamzabat.algohub.repository.GroupMemberRepository;
 import com.gamzabat.algohub.repository.StudyGroupRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,15 +32,29 @@ class StudyGroupServiceTest {
 	@Mock
 	private StudyGroupRepository studyGroupRepository;
 	@Mock
+	private GroupMemberRepository groupMemberRepository;
+	@Mock
 	private ImageService imageService;
 	private User user;
+	private User user2;
+	private StudyGroup group;
 	@Captor
 	private ArgumentCaptor<StudyGroup> groupCaptor;
+	@Captor
+	private ArgumentCaptor<GroupMember> memberCaptor;
 
 	@BeforeEach
-	void setUp() {
-		user = User.builder().email("email").password("password").nickname("nickname")
+	void setUp() throws NoSuchFieldException, IllegalAccessException {
+		user = User.builder().email("email1").password("password").nickname("nickname")
 			.role(Role.USER).profileImage("image").build();
+		user2 = User.builder().email("email2").password("password").nickname("nickname")
+			.role(Role.USER).profileImage("image").build();
+		group = StudyGroup.builder().name("name").owner(user).groupImage("imageUrl").groupCode("code").build();
+
+		Field userField = User.class.getDeclaredField("id");
+		userField.setAccessible(true);
+		userField.set(user,1L);
+		userField.set(user2,2L);
 	}
 
 	@Test
@@ -54,4 +74,52 @@ class StudyGroupServiceTest {
 		assertThat(result.getOwner()).isEqualTo(user);
 		assertThat(result.getGroupImage()).isEqualTo(imageUrl);
 	}
+
+	@Test
+	@DisplayName("코드 사용한 그룹 참여 성공")
+	void joinGroupWithCode(){
+		// given
+		when(studyGroupRepository.findByGroupCode("code")).thenReturn(Optional.ofNullable(group));
+		// when
+		studyGroupService.joinGroupWithCode(user2,"code");
+		// then
+		verify(groupMemberRepository,times(1)).save(memberCaptor.capture());
+		GroupMember result = memberCaptor.getValue();
+		assertThat(result.getStudyGroup()).isEqualTo(group);
+		assertThat(result.getUser()).isEqualTo(user2);
+	}
+
+	@Test
+	@DisplayName("코드 사용한 그룹 참여 실패 : 존재하지 않는 그룹")
+	void joinGroupWithCodeFailed_1(){
+		// given
+		when(studyGroupRepository.findByGroupCode("code")).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> studyGroupService.joinGroupWithCode(user2,"code"))
+			.isInstanceOf(StudyGroupValidationException.class)
+			.hasFieldOrPropertyWithValue("error","존재하지 않는 그룹 입니다.");
+	}
+	@Test
+	@DisplayName("코드 사용한 그룹 참여 실패 : 이미 참여한 그룹 (주인)")
+	void joinGroupWithCodeFailed_2(){
+		// given
+		when(studyGroupRepository.findByGroupCode("code")).thenReturn(Optional.ofNullable(group));
+		// when, then
+		assertThatThrownBy(() -> studyGroupService.joinGroupWithCode(user,"code"))
+			.isInstanceOf(StudyGroupValidationException.class)
+			.hasFieldOrPropertyWithValue("error","이미 참여한 그룹 입니다.");
+	}
+	@Test
+	@DisplayName("코드 사용한 그룹 참여 실패 : 이미 참여한 그룹 (멤버)")
+	void joinGroupWithCodeFailed_3(){
+		// given
+		when(studyGroupRepository.findByGroupCode("code")).thenReturn(Optional.ofNullable(group));
+		when(groupMemberRepository.existsByUserAndStudyGroup(user2,group)).thenReturn(true);
+		// when, then
+		assertThatThrownBy(() -> studyGroupService.joinGroupWithCode(user2,"code"))
+			.isInstanceOf(StudyGroupValidationException.class)
+			.hasFieldOrPropertyWithValue("error","이미 참여한 그룹 입니다.");
+	}
+
+
 }
