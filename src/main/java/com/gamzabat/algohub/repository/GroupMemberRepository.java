b2dbe38a94b62645751377ca5a146af1fb914cfd
rladmstn <oz33177351@gmail.com>
2024-07-12 19:34:12 +0900

    fix : GroupMemberRepository 메서드 오류 수정

--- src/main/java/com/gamzabat/algohub/repository/GroupMemberRepository.java
+++ src/main/java/com/gamzabat/algohub/repository/GroupMemberRepository.java
@@ -1,6 +1,7 @@
 package com.gamzabat.algohub.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,7 +11,7 @@ import com.gamzabat.algohub.domain.User;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 	boolean existsByUserAndStudyGroup(User user, StudyGroup studyGroup);
-	List<GroupMember> findByUserAndStudyGroup(User user, StudyGroup studyGroup);
+	Optional<GroupMember> findByUserAndStudyGroup(User user, StudyGroup studyGroup);
 	List<GroupMember> findAllByStudyGroup(StudyGroup studyGroup);
 	boolean existsByUser(User user);
 }
