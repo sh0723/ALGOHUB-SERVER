9505a9e843b6144f148d5957fb3e9598f23fe731
rladmstn <oz33177351@gmail.com>
2024-07-12 16:08:27 +0900

    feat : 풀이 별 댓글 전체 조회 기능 추가
    풀이 하나에 대한 댓글 전체 조회 기능 추가

--- src/main/java/com/gamzabat/algohub/controller/CommentController.java
+++ src/main/java/com/gamzabat/algohub/controller/CommentController.java
@@ -1,15 +1,21 @@
 package com.gamzabat.algohub.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamzabat.algohub.common.annotation.AuthedUser;
+import com.gamzabat.algohub.domain.Solution;
 import com.gamzabat.algohub.domain.User;
 import com.gamzabat.algohub.dto.CreateCommentRequest;
+import com.gamzabat.algohub.dto.GetCommentResponse;
 import com.gamzabat.algohub.exception.RequestException;
 import com.gamzabat.algohub.service.CommentService;
 
@@ -33,5 +39,12 @@ public class CommentController {
 			throw new RequestException("댓글 작성 요청이 올바르지 않습니다.",errors);
 		commentService.createComment(user,request);
 		return ResponseEntity.ok().body("OK");
+	}
+
+	@GetMapping
+	@Operation(summary = "댓글 목록 조회 API", description = "풀이 하나에 대한 댓글 전체 조회")
+	public ResponseEntity<List<GetCommentResponse>> getCommentList(@AuthedUser User user, @RequestParam Long solutionId){
+		List<GetCommentResponse> response = commentService.getCommentList(user,solutionId);
+		return ResponseEntity.ok().body(response);
 	}
 }
--- src/main/java/com/gamzabat/algohub/service/CommentService.java
+++ src/main/java/com/gamzabat/algohub/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.gamzabat.algohub.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import com.gamzabat.algohub.domain.Solution;
 import com.gamzabat.algohub.domain.StudyGroup;
 import com.gamzabat.algohub.domain.User;
 import com.gamzabat.algohub.dto.CreateCommentRequest;
+import com.gamzabat.algohub.dto.GetCommentResponse;
 import com.gamzabat.algohub.exception.GroupMemberValidationException;
 import com.gamzabat.algohub.exception.ProblemValidationException;
 import com.gamzabat.algohub.exception.SolutionValidationException;
@@ -36,7 +38,27 @@ public class CommentService {
 	private final StudyGroupRepository studyGroupRepository;
 	private final GroupMemberRepository groupMemberRepository;
 	public void createComment(User user, CreateCommentRequest request) {
-		Solution solution = solutionRepository.findById(request.solutionId())
+		Solution solution = checkSolutionValidation(user, request.solutionId());
+
+		commentRepository.save(Comment.builder()
+				.user(user)
+				.solution(solution)
+				.content(request.content())
+				.createdAt(LocalDateTime.now())
+			.build());
+		log.info("success to create comment");
+	}
+
+	public List<GetCommentResponse> getCommentList(User user, Long solutionId) {
+		Solution solution = checkSolutionValidation(user, solutionId);
+		List<Comment> list = commentRepository.findAllBySolution(solution);
+		List<GetCommentResponse> result = list.stream().map(GetCommentResponse::toDTO).toList();
+		log.info("success to get comment list");
+		return result;
+	}
+
+	private Solution checkSolutionValidation(User user, Long solutionId) {
+		Solution solution = solutionRepository.findById(solutionId)
 			.orElseThrow(() -> new SolutionValidationException("존재하지 않는 풀이 입니다."));
 
 		Problem problem = problemRepository.findById(solution.getProblem().getId())
@@ -49,12 +71,6 @@ public class CommentService {
 			&& !group.getOwner().getId().equals(user.getId()))
 			throw new GroupMemberValidationException(HttpStatus.FORBIDDEN.value(),"참여하지 않은 그룹 입니다.");
 
-		commentRepository.save(Comment.builder()
-				.user(user)
-				.solution(solution)
-				.content(request.content())
-				.createdAt(LocalDateTime.now())
-			.build());
-		log.info("success to create comment");
+		return solution;
 	}
 }
