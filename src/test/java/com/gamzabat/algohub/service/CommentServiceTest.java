6039fb15a88c3c95d88db472159b2c6db0ab9348
rladmstn <oz33177351@gmail.com>
2024-07-12 17:07:15 +0900

    test : 댓글 삭제 테스트 작성
    댓글 삭제 성공, 존재하지 않는 댓글/그룹/문제/풀이, 권한 없음에 대한 실패 테스트 작성

--- src/test/java/com/gamzabat/algohub/service/CommentServiceTest.java
+++ src/test/java/com/gamzabat/algohub/service/CommentServiceTest.java
@@ -27,6 +27,7 @@ import com.gamzabat.algohub.domain.User;
 import com.gamzabat.algohub.dto.CreateCommentRequest;
 import com.gamzabat.algohub.dto.GetCommentResponse;
 import com.gamzabat.algohub.enums.Role;
+import com.gamzabat.algohub.exception.CommentValidationException;
 import com.gamzabat.algohub.exception.GroupMemberValidationException;
 import com.gamzabat.algohub.exception.ProblemValidationException;
 import com.gamzabat.algohub.exception.SolutionValidationException;
@@ -52,6 +53,7 @@ class CommentServiceTest {
 	@Mock
 	private ProblemRepository problemRepository;
 	private User user, user2;
+	private Comment comment, comment2;
 	private Solution solution;
 	private Problem problem;
 	private StudyGroup studyGroup;
@@ -66,10 +68,13 @@ class CommentServiceTest {
 		studyGroup = StudyGroup.builder().owner(user).build();
 		problem = Problem.builder().studyGroup(studyGroup).build();
 		solution = Solution.builder().problem(problem).content("solution").build();
+		comment = Comment.builder().user(user).content("content").solution(solution).build();
+		comment2 = Comment.builder().user(user2).content("content").solution(solution).build();
 
 		Field userField = User.class.getDeclaredField("id");
 		userField.setAccessible(true);
 		userField.set(user,1L);
+		userField.set(user2,2L);
 
 		Field solutionField = Solution.class.getDeclaredField("id");
 		solutionField.setAccessible(true);
@@ -82,6 +87,11 @@ class CommentServiceTest {
 		Field groupField = StudyGroup.class.getDeclaredField("id");
 		groupField.setAccessible(true);
 		groupField.set(studyGroup,30L);
+
+		Field commentField = Comment.class.getDeclaredField("id");
+		commentField.setAccessible(true);
+		commentField.set(comment,40L);
+		commentField.set(comment2,41L);
 	}
 
 	@Test
@@ -245,7 +255,6 @@ class CommentServiceTest {
 	@DisplayName("댓글 조회 실패 : 존재하지 않는 그룹")
 	void getCommentListFailed_3(){
 		// given
-		CreateCommentRequest request = CreateCommentRequest.builder().solutionId(10L).content("content").build();
 		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
 		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
 		when(studyGroupRepository.findById(30L)).thenReturn(Optional.empty());
@@ -260,13 +269,122 @@ class CommentServiceTest {
 	@DisplayName("댓글 조회 실패 : 참여하지 않은 그룹")
 	void getCommentListFailed_4(){
 		// given
-		CreateCommentRequest request = CreateCommentRequest.builder().solutionId(10L).content("content").build();
 		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
 		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
 		when(studyGroupRepository.findById(30L)).thenReturn(Optional.ofNullable(studyGroup));
 		when(groupMemberRepository.existsByUserAndStudyGroup(user2,studyGroup)).thenReturn(false);
 		// when, then
 		assertThatThrownBy(() -> commentService.getCommentList(user2,10L))
+			.isInstanceOf(GroupMemberValidationException.class)
+			.hasFieldOrPropertyWithValue("code", HttpStatus.FORBIDDEN.value())
+			.hasFieldOrPropertyWithValue("error","참여하지 않은 그룹 입니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 성공 (주인)")
+	void deleteComment_1() {
+		// given
+		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(studyGroupRepository.findById(30L)).thenReturn(Optional.ofNullable(studyGroup));
+		when(commentRepository.findById(40L)).thenReturn(Optional.ofNullable(comment));
+		// when
+		commentService.deleteComment(user, 40L);
+		// then
+		verify(commentRepository,times(1)).delete(comment);
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 성공 (멤버)")
+	void deleteComment_2() {
+		// given
+		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(studyGroupRepository.findById(30L)).thenReturn(Optional.ofNullable(studyGroup));
+		when(groupMemberRepository.existsByUserAndStudyGroup(user2,studyGroup)).thenReturn(true);
+		when(commentRepository.findById(41L)).thenReturn(Optional.ofNullable(comment2));
+		// when
+		commentService.deleteComment(user2, 41L);
+		// then
+		verify(commentRepository,times(1)).delete(comment2);
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 존재하지 않는 댓글")
+	void deleteCommentFailed_1(){
+		// given
+		when(commentRepository.findById(40L)).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user,40L))
+			.isInstanceOf(CommentValidationException.class)
+			.hasFieldOrPropertyWithValue("code",HttpStatus.NOT_FOUND.value())
+			.hasFieldOrPropertyWithValue("error","존재하지 않는 댓글 입니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 댓글 삭제 권한 없음")
+	void deleteCommentFailed_2(){
+		// given
+		when(commentRepository.findById(40L)).thenReturn(Optional.ofNullable(comment));
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user2,40L))
+			.isInstanceOf(CommentValidationException.class)
+			.hasFieldOrPropertyWithValue("code",HttpStatus.FORBIDDEN.value())
+			.hasFieldOrPropertyWithValue("error","댓글 삭제에 대한 권한이 없습니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 존재하지 않는 풀이")
+	void deleteCommentFailed_3(){
+		// given
+		when(commentRepository.findById(40L)).thenReturn(Optional.ofNullable(comment));
+		when(solutionRepository.findById(10L)).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user,40L))
+			.isInstanceOf(SolutionValidationException.class)
+			.hasFieldOrPropertyWithValue("error","존재하지 않는 풀이 입니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 존재하지 않는 문제")
+	void deleteCommentFailed_4(){
+		// given
+		when(commentRepository.findById(40L)).thenReturn(Optional.ofNullable(comment));
+		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
+		when(problemRepository.findById(20L)).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user,40L))
+			.isInstanceOf(ProblemValidationException.class)
+			.hasFieldOrPropertyWithValue("code", HttpStatus.NOT_FOUND.value())
+			.hasFieldOrPropertyWithValue("error","존재하지 않는 문제 입니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 존재하지 않는 그룹")
+	void deleteCommentFailed_5(){
+		// given
+		when(commentRepository.findById(40L)).thenReturn(Optional.ofNullable(comment));
+		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(studyGroupRepository.findById(30L)).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user,40L))
+			.isInstanceOf(StudyGroupValidationException.class)
+			.hasFieldOrPropertyWithValue("code", HttpStatus.NOT_FOUND.value())
+			.hasFieldOrPropertyWithValue("error","존재하지 않는 그룹 입니다.");
+	}
+
+	@Test
+	@DisplayName("댓글 삭제 실패 : 참여하지 않은 그룹")
+	void deleteCommentFailed_6(){
+		// given
+		when(commentRepository.findById(41L)).thenReturn(Optional.ofNullable(comment2));
+		when(solutionRepository.findById(10L)).thenReturn(Optional.ofNullable(solution));
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(studyGroupRepository.findById(30L)).thenReturn(Optional.ofNullable(studyGroup));
+		when(groupMemberRepository.existsByUserAndStudyGroup(user2,studyGroup)).thenReturn(false);
+		// when, then
+		assertThatThrownBy(() -> commentService.deleteComment(user2,41L))
 			.isInstanceOf(GroupMemberValidationException.class)
 			.hasFieldOrPropertyWithValue("code", HttpStatus.FORBIDDEN.value())
 			.hasFieldOrPropertyWithValue("error","참여하지 않은 그룹 입니다.");
