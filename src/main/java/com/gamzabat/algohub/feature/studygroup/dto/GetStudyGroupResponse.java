abffdd7abe632709c19e6dfa8a69f6b8f97428ce
rladmstn <oz33177351@gmail.com>
2024-07-13 22:44:14 +0900

    feat : 스터디 그룹 목록 조회에 스터디 그룹 진행 기간, 한 줄 소개 추가

--- src/main/java/com/gamzabat/algohub/feature/studygroup/dto/GetStudyGroupResponse.java
+++ src/main/java/com/gamzabat/algohub/feature/studygroup/dto/GetStudyGroupResponse.java
@@ -1,11 +1,16 @@
 package com.gamzabat.algohub.feature.studygroup.dto;
 
+import java.time.LocalDate;
+
 import com.gamzabat.algohub.feature.studygroup.domain.StudyGroup;
 import com.gamzabat.algohub.feature.user.domain.User;
 
 public record GetStudyGroupResponse(Long id,
 									String name,
 									String groupImage,
+									LocalDate startDate,
+									LocalDate endDate,
+									String introduction,
 									String ownerNickname,
 									boolean isOwner) {
 	public static GetStudyGroupResponse toDTO(StudyGroup group, User user){
@@ -13,6 +18,9 @@ public record GetStudyGroupResponse(Long id,
 			group.getId(),
 			group.getName(),
 			group.getGroupImage(),
+			group.getStartDate(),
+			group.getEndDate(),
+			group.getIntroduction(),
 			group.getOwner().getNickname(),
 			group.getOwner().getId().equals(user.getId())
 		);
