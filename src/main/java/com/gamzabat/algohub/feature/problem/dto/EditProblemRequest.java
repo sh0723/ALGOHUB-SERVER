63dbd77cfcd144e680e9e37b42cc7389e5dd1cd4
rladmstn <oz33177351@gmail.com>
2024-07-21 23:07:38 +0900

    feat : 문제 기간 수정 valid 수정

--- src/main/java/com/gamzabat/algohub/feature/problem/dto/EditProblemRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/problem/dto/EditProblemRequest.java
@@ -7,6 +7,6 @@ import lombok.Builder;
 
 @Builder
 public record EditProblemRequest(@NotNull(message = "문제 고유 아이디는 필수 입력 입니다.") Long problemId,
-								 @NotNull(message = "시작 날짜는 필수 입력 입니다.") LocalDate startDate,
-								 @NotNull(message = "마감 날짜는 필수 입력 입니다.") LocalDate endDate) {
+								 LocalDate startDate,
+								 LocalDate endDate) {
 }
