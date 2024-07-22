d2a9ed6964e81485f96ee19279dd32dc3e872aef
s-hwan <tmdghks507@daum.net>
2024-07-23 00:07:04 +0900

    refactor : solution 가독성 고려 수정

--- src/main/java/com/gamzabat/algohub/feature/solution/dto/CreateSolutionRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/solution/dto/CreateSolutionRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record CreateSolutionRequest(@NotNull(message = "아이디 입력은 필수 입력 입니다.") String userName,
 									String code,
-									@NotNull(message = "문제 번호 입력은 필수 입력 입니다.") Integer problemNummber,
+									@NotNull(message = "문제 번호 입력은 필수 입력 입니다.") Integer problemNumber,
 									@NotNull(message = "제출 번호 입력은 필수 입력 입니다.") String submissionId) {
 }
