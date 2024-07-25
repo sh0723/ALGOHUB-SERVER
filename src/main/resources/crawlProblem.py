c9d6e52f49b9adf05ef03c7658915d38aa1dd223
s-hwan <tmdghks507@daum.net>
2024-07-25 23:30:26 +0900

    refactor:크롤러 백그라운드 실행 추가

--- src/main/resources/crawlProblem.py
+++ src/main/resources/crawlProblem.py
@@ -1,34 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import json
 import sys
 import io
 
+
 def searchProblem(userId, problemNumber, targetSubmissionId):
-    # WebDriver 설정
+
+
+    # ChromeOptions 객체 생성 및 헤드리스 모드 설정
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")  # 화면 크기 설정
+    options.add_argument("--disable-blink-features=AutomationControlled")  # 자동화 제어 방지
+    options.add_argument("headless")  # 헤드리스 모드 활성화
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36");
+
+    # Web Driver 설정
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
+
 
     try:
         # BOJ 사이트 접속
         driver.get('https://www.acmicpc.net/status')
 
         # 페이지가 로드될 때까지 기다리기
-        time.sleep(0.2)
+        time.sleep(5)
 
         # 문제 검색창 찾기 (NAME 속성을 사용하여)
-        searchBox = driver.find_element(By.NAME, 'problem_id')
+        searchBox = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[4]/div/form/input[1]')
 
         # 문제 번호 입력
         searchBox.send_keys(problemNumber)
 
         # 아이디 검색창 찾기 (NAME 속성을 사용하여)
-        searchBoxId = driver.find_element(By.NAME, 'user_id')
-
+        searchBoxId = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[4]/div/form/input[2]')
         # id 입력
         searchBoxId.send_keys(userId)
 
@@ -36,7 +52,7 @@ def searchProblem(userId, problemNumber, targetSubmissionId):
         searchBox.send_keys(Keys.RETURN)
 
         # 검색 결과 로딩 기다리기
-        time.sleep(0.2)
+        time.sleep(5)
 
         # userId와 problemNumber로 가져온 제출 기록 찾기
         rows = driver.find_elements(By.CSS_SELECTOR, 'tbody tr')
