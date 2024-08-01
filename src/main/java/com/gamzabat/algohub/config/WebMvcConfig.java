c34ea77b3e63d7fcaaa0715d20183a919a5dad21
rladmstn <oz33177351@gmail.com>
2024-08-01 23:39:17 +0900

    fix : cors method 에러 해결

--- src/main/java/com/gamzabat/algohub/config/WebMvcConfig.java
+++ src/main/java/com/gamzabat/algohub/config/WebMvcConfig.java
@@ -23,6 +23,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+			.allowedMethods("GET","POST","PATCH","PUT","DELETE");
 	}
 }
