0e5c29c8def830b00864d56123f69ac1237c81c0
rladmstn <oz33177351@gmail.com>
2024-08-11 21:44:54 +0900

    refactor : redis 인터페이스 삭제
    기능 확장 고려 판단에 따라 redis 인터페이스 삭제, 클래스로 구현

--- src/main/java/com/gamzabat/algohub/common/redis/RedisService.java
+++ src/main/java/com/gamzabat/algohub/common/redis/RedisService.java
@@ -1,13 +1,32 @@
 package com.gamzabat.algohub.common.redis;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
 @Service
-public interface RedisService {
-    void setValues(String key, String value);
-    void setValues(String key, String value, Duration duration);
-    String getValues(String key);
-    void deleteValues(String key);
+@RequiredArgsConstructor
+public class RedisService{
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void setValues(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void setValues(String key, String value, Duration duration) {
+        redisTemplate.opsForValue().set(key,value,duration);
+    }
+
+    public String getValues(String key) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        if(values.get(key) == null) return "";
+        return String.valueOf(values.get(key));
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
 }
